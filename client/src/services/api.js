b7ed1dff55a2222ff5f32f11299abc4872750bb4
Anup Kumar <anupg2551@gmail.com>
2026-02-02 15:50:37 +0530

    Updated API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 
 
-// https://code-mastery-backend.vercel.app/
+// https://code-mastery-backend.vercel.app/api
 // http://localhost:5000/api
-const API_URL = 'https://code-mastery-backend.vercel.app';
+const API_URL = 'https://code-mastery-backend.vercel.app/api';
 
 const api = axios.create({
     baseURL: API_URL,
